f800fa7a8edfa3bd7cdfe31cb3f69b3263fc5df5
alex404A <maxmalex1990@gmail.com>
2020-12-09 17:00:32 +0800

    solve 7-easy-readonly

--- questions/7-easy-readonly/test-cases.ts
+++ questions/7-easy-readonly/test-cases.ts
@@ -1,11 +1,11 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
 
-type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+type MyReadonly<T> = { readonly [K in keyof T]: T[K] };
+
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
 
 interface Todo1 {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
 }
