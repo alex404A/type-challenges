a19baa4c84dc1425e4a24298ca2705c2d361791c
alex404A <maxmalex1990@gmail.com>
2020-12-09 16:19:10 +0800

    solve 4-easy-pick

--- questions/4-easy-pick/test-cases.ts
+++ questions/4-easy-pick/test-cases.ts
@@ -1,21 +1,23 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
+
+type MyPick<K, U extends keyof K> = { [S in U]: K[S] };
 
 type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-]
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>
+];
 
 interface Todo {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
 }
 
 interface Expected1 {
-  title: string
+  title: string;
 }
 
 interface Expected2 {
-  title: string
-  completed: boolean
+  title: string;
+  completed: boolean;
 }
