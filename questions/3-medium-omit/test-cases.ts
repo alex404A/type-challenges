81818741b001ddee40a726df23d8b757b0e0c498
alex404A <maxmalex1990@gmail.com>
2020-12-09 16:54:57 +0800

    solve 3-medium-omit

--- questions/3-medium-omit/test-cases.ts
+++ questions/3-medium-omit/test-cases.ts
@@ -1,21 +1,24 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
+
+type MyExclude<A, B> = A extends B ? never : A;
+type MyOmit<K, U extends keyof K> = { [S in MyExclude<keyof K, U>]: K[S] };
 
 type cases = [
-  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
-  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>
-]
+  Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
+];
 
 interface Todo {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
 }
 
 interface Expected1 {
-  title: string
-  completed: boolean
+  title: string;
+  completed: boolean;
 }
 
 interface Expected2 {
-  title: string
+  title: string;
 }
