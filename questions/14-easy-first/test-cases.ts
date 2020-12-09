c1884f35f0f99898ca8106a1713794365a781743
alex404A <maxmalex1990@gmail.com>
2020-12-09 18:37:39 +0800

    solve 14-easy-first

--- questions/14-easy-first/test-cases.ts
+++ questions/14-easy-first/test-cases.ts
@@ -1,7 +1,9 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
+
+type First<T extends any[]> = T extends [infer F, ...any[]] ? F : never;
 
 type cases = [
   Expect<Equal<First<[3, 2, 1]>, 3>>,
   Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
   Expect<Equal<First<[]>, never>>
-]
+];
