6146e28def72be0f660dd9c6a7acfe76f3f99bda
alex404A <maxmalex1990@gmail.com>
2020-12-10 19:18:05 +0800

    solve 14-easy-tuple-length

--- questions/18-easy-tuple-length/test-cases.ts
+++ questions/18-easy-tuple-length/test-cases.ts
@@ -1,9 +1,20 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
 
-const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
-const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+type Length<
+  T extends { readonly [a: number]: any; readonly length: number }
+> = T["length"];
+
+const tesla = ["tesla", "model 3", "model X", "model Y"] as const;
+const spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT",
+] as const;
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
-  Expect<Equal<Length<typeof spaceX>, 5>>,
-]
+  Expect<Equal<Length<typeof spaceX>, 5>>
+];
+
