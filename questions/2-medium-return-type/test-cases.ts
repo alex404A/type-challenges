fe44f8a33a7900b1e0db80f5b3ab0c1ca37ab428
alex404A <maxmalex1990@gmail.com>
2020-12-10 19:56:10 +0800

    solve 2-medium-return-type

--- questions/2-medium-return-type/test-cases.ts
+++ questions/2-medium-return-type/test-cases.ts
@@ -1,18 +1,24 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
+
+// type MyReturnType<T extends () => {}> = T extends (...args: any[]) => infer R ? R : never
+
+type MyReturnType<T extends Function> = T extends (...args: any) => infer R
+  ? R
+  : never;
 
 type cases = [
   Expect<Equal<string, MyReturnType<() => string>>>,
   Expect<Equal<123, MyReturnType<() => 123>>>,
   Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
   Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
-  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
-  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
-]
+  Expect<Equal<() => "foo", MyReturnType<() => () => "foo">>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>
+];
 
 type ComplexObject = {
-  a: [12, 'foo']
-  bar: 'hello'
-  prev(): number
-}
+  a: [12, "foo"];
+  bar: "hello";
+  prev(): number;
+};
 
-const fn = (v: boolean) => v ? 1 : 2
+const fn = (v: boolean) => (v ? 1 : 2);
