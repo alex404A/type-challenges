4ea3e1ec239fbb5bf4e243aedeec8759f75e64e1
alex404A <maxmalex1990@gmail.com>
2020-12-10 19:20:02 +0800

    solve 43-easy-exclude

--- questions/43-easy-exclude/test-cases.ts
+++ questions/43-easy-exclude/test-cases.ts
@@ -1,7 +1,19 @@
-import { Equal, Expect, ExpectFalse, NotEqual } from '../../utils'
+import { Equal, Expect, ExpectFalse, NotEqual } from "../../utils";
+
+type MyExclude<A, B> = A extends B ? never : A;
 
 type cases = [
-    Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
-    Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, Exclude<"a" | "b" | "c", "a" | "b">>>,
-    Expect<Equal<MyExclude<string | number | (() => void), Function>, Exclude<string | number | (() => void), Function>>>,
-]
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
+  Expect<
+    Equal<
+      MyExclude<"a" | "b" | "c", "a" | "b">,
+      Exclude<"a" | "b" | "c", "a" | "b">
+    >
+  >,
+  Expect<
+    Equal<
+      MyExclude<string | number | (() => void), Function>,
+      Exclude<string | number | (() => void), Function>
+    >
+  >
+];
