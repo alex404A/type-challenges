218f2e202af2da15ae01fbf9e2c37c1b6a6ca5e2
alex404A <maxmalex1990@gmail.com>
2020-12-10 20:08:13 +0800

    solve 10-medium-tuple-to-union

--- questions/10-medium-tuple-to-union/test-cases.ts
+++ questions/10-medium-tuple-to-union/test-cases.ts
@@ -1,6 +1,11 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
+
+type TupleToUnion<T extends any[]> = T[number];
+type TupleToUnion2<T extends any[]> = T extends Array<infer ITEMS>
+  ? ITEMS
+  : never;
 
 type cases = [
-  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
-  Expect<Equal<TupleToUnion<[123]>, 123>>,
-]
+  Expect<Equal<TupleToUnion<[123, "456", true]>, 123 | "456" | true>>,
+  Expect<Equal<TupleToUnion2<[123]>, 123>>
+];
