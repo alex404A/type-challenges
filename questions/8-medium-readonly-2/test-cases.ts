2f0a0673dbf8190c2007eb6f2db1290eb7b4544b
alex404A <maxmalex1990@gmail.com>
2020-12-10 20:04:13 +0800

    solve 8-medium-readonly-2

--- questions/8-medium-readonly-2/test-cases.ts
+++ questions/8-medium-readonly-2/test-cases.ts
@@ -1,18 +1,23 @@
-import { Equal, Expect } from '../../utils'
+import { Equal, Expect } from "../../utils";
+
+type Combine<T> = { [k in keyof T]: T[k] };
+type MyReadonly2<T, K extends keyof T = keyof T> = Combine<
+  T & { readonly [S in K]: T[S] }
+>;
 
 type cases = [
   Expect<Equal<MyReadonly2<Todo1>, Readonly<Todo1>>>,
-  Expect<Equal<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
-]
+  Expect<Equal<MyReadonly2<Todo1, "title" | "description">, Expected>>
+];
 
 interface Todo1 {
-  title: string
-  description?: string
-  completed: boolean
+  title: string;
+  description?: string;
+  completed: boolean;
 }
 
 interface Expected {
-  readonly title: string
-  readonly description?: string
-  completed: boolean
+  readonly title: string;
+  readonly description?: string;
+  completed: boolean;
 }
